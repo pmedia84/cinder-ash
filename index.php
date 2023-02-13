6948deb2e31fde2490855728d0f8f2a4ed720c3e
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2023-02-13 14:19:01 +0000

    temp page

--- index.php
+++ index.php
@@ -5,9 +5,28 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/styles.css">
-    <title>Document</title>
+        <!-- Google Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Alex+Brush&family=Glory:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,800&display=swap" rel="stylesheet">
+    <title>Cinder Ash Pre School</title>
 </head>
 <body>
-    <h1>Hello World</h1>
+  
+        <section class="container home">
+            <div class="home-temp">
+                <div class="home-temp-col my-3">
+                    <h1>Welcome to Cinder Ash Pre School</h1>
+                    <p>Our website is undergoing maintenance </p>
+                    <p>If you would like to get in touch with us, you can contact us:</p>
+                    <p><strong>Phone: </strong>01406 258382</p>
+                    <p><strong>email: </strong>cinderashpre-school@hotmail.co.uk</p>
+                </div>
+                <div class="home-temp-col my-3 end">
+                    <img src="assets/img/home-temp-graphic.jpg" alt="">
+                </div>
+            </div>
+        </section>
+   
 </body>
 </html>
