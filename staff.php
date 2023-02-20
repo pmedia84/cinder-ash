daf07e7e8908639b380dfd47ccccc9c5dc54a729
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2023-02-20 14:17:57 +0000

    staff page

---
+++ staff.php
@@ -0,0 +1,165 @@
+<?php include("inc/header.inc.php"); ?>
+
+<!-- Page specific information and scripts -->
+<title>Cinder Ash - Nursery and pre school in Long Sutton - Meet Our Team</title>
+
+</head>
+
+<body itemscope itemtype="https://schema.org/ChildCare">
+    <?php include("inc/nav.inc.php"); ?>
+    <main>
+        <div class="hero about-hero">
+            <div class="hero-container">
+                <h1 class="hero-title">Meet the Team</h1>
+                <p class="hero-subtitle">Get to know the team behind our nursery</p>
+
+            </div>
+        </div>
+
+        <section class="container my-4">
+            <h2 class="section-title text-center">Meet The Team</h2>
+            <div class="std-card">
+                <div class="grid-row-3col my-2">
+                    <div class="staff-card-lg">
+                        <div class="staff-card-header">
+                            <img src="assets/img/staff/lucy.jpg" alt="">
+                        </div>
+                        <div class="staff-card-body">
+                            <p class="staff-card-name text-center">Lucy Cooper</p>
+                            <p class="staff-card-title text-center">Manager</p>
+                            <p class="staff-card-text">Lucy has been our manager for eight years now, joining us after previous experience of both nurseries and primary schools. Lucy has her BA (hons) degree in Early Years and has used this to influence changeover the setting. She has done a variety of CPD courses including safeguarding level 2 and inter agency training, making her designated safeguarding lead for the setting. She has also completed her SENCo training, alongside a range of other SEN qualifications and has previous experience within the SENCo role. She has also completed beginning with babies training, PEEP training, and courses promoting an improvement in literacy in maths. She completed training to specialise in working with children with english as an additional language and this has been really influential in practice. She has both paediatric and adult first aid training, alongside prevent duty awareness and a range of other training. </p>
+                            <div class="text-center my-2"><button class="btn-primary staff-text">Read More</button></div>
+                        </div>
+                    </div>
+                    <div class="staff-card-lg">
+                        <div class="staff-card-header">
+                            <img src="assets/img/staff/charlotte.jpg" alt="">
+                        </div>
+                        <div class="staff-card-body">
+                            <p class="staff-card-name text-center">Charlotte</p>
+                            <p class="staff-card-title text-center">Deputy Manager &amp; SENCo</p>
+                            <p class="staff-card-text">Charlotte is our deputy manager, taking on the role if manager in the absence of Lucy and Melissa. She is also our setting SENCo and takes leadership on ensuring the needs of our children with additional needs are met, and putting the best package of support as possible in place. Within this role she supports children on an individual basis, but also with small cluster group intervention times where extra support has been identified. Charlotte has worked in both nurseries and in a nannying role, qualified to QTS level, with a PGCE in primary education. She has full and relevant SENCo training, and a variety of CPD courses. She has both paediatric and emergency at work first aid training. She has a huge interest in superhero’s and a fantastic imagination which is very much enjoyed by all the children at Cinder Ash. </p>
+                            <div class="text-center my-2"><button class="btn-primary staff-text">Read More</button></div>
+                        </div>
+                    </div>
+                    <div class="staff-card-lg">
+                        <div class="staff-card-header">
+                            <img src="assets/img/staff/melissa.jpg" alt="">
+                        </div>
+                        <div class="staff-card-body">
+                            <p class="staff-card-name text-center">Melissa</p>
+                            <p class="staff-card-title text-center">Floor Manager</p>
+                            <p class="staff-card-text">Melissa is our floor manager and joined the setting back in April 2016. Prior to joining us, Melissa has experience working in nursery settings, as well as a specialist provision school. During her time within a specialist provision school. she gained lots of experience working with children with a range of additional needs, using equipment appropriate to their requirements, and supporting them in the most suitable way to gain the full potential from their learning. Melissa has full SENCo training, and initially was in the role of SENCo within the setting, before taking on the deputy manager position. She has completed safeguarding level 2 and inter agency training, alongside both adult and paediatric first aid training. She has a wide and varied range of CPD courses which she has completed, and influence her every day practise within the setting.</p>
+                            <div class="text-center my-2"><button class="btn-primary staff-text">Read More</button></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="grid-row-3col">
+                    <div class="staff-card-lg">
+                        <div class="staff-card-header">
+                            <img src="assets/img/staff/amy.jpg" alt="">
+                        </div>
+                        <div class="staff-card-body">
+                            <p class="staff-card-name text-center">Amy</p>
+                            <p class="staff-card-title text-center">Early Years Practitioner</p>
+                            <p class="staff-card-text">Amy joined us here at Cinder Ash in March 2020. She has a BA(hons) degree in a and youth studies, alongside her NNEB qualification. Amy has worked previously as a childminder and also within a variety of nurseries and also a primary school. Amy is working alongside Charlotte as our SENCo, and is currently completing her SENCo training to provide further support in this field. She holds paediatric first aid and relevant safeguarding training. She is proactive in supporting outdoor learning and ensuring learning opportunities are accessible to all. </p>
+                            <div class="text-center my-2"><button class="btn-primary staff-text">Read More</button></div>
+                        </div>
+                    </div>
+                    <div class="staff-card-lg">
+                        <div class="staff-card-header">
+                            <img src="assets/img/staff/beth.jpg" alt="">
+                        </div>
+                        <div class="staff-card-body">
+                            <p class="staff-card-name text-center">Beth</p>
+                            <p class="staff-card-title text-center">Early Years Practitioner</p>
+                            <p class="staff-card-text">Beth initially joined us as an apprentice in February 2017, and has continued with us post qualifying as a level 3 practitioner and is a hugely popular member of the team with the children. We very much follow a choosing your own key person approach and Beth has a considerably high number of key children who have chosen her as their ‘special nursery person’! She predominantly works with our younger children, providing holistic learning opportunities and sensory experiences. Beth is qualified in paediatric first aid and relevant safeguarding, and has done a range of CPD training focussing on the needs of under 2’s. </p>
+                            <div class="text-center my-2"><button class="btn-primary staff-text">Read More</button></div>
+                        </div>
+                    </div>
+                    <div class="staff-card-lg">
+                        <div class="staff-card-header">
+                            <img src="assets/img/staff/laura.jpg" alt="">
+                        </div>
+                        <div class="staff-card-body">
+                            <p class="staff-card-name text-center">Laura</p>
+                            <p class="staff-card-title text-center">Early Years Practitioner</p>
+                            <p class="staff-card-text">Laura joined the team initially in October 2018 as a level 3 apprentice, before continuing with us as a qualified member of staff. She has always had a passion for working with children with previous roles in nurseries, and this is demonstrated with her wonderful rapport she has build with the children at setting. She has a huge interest in the natural environment, and provides wonderful learning opportunities such as  the growing of fruits and vegetables, creating environments and food for wildlife and looking at ways we can care for the World around us. Laura holds all relevant safeguarding qualifications and also her paediatric first aid. </p>
+                            <div class="text-center my-2"><button class="btn-primary staff-text">Read More</button></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="grid-row-3col">
+                <div class="staff-card-lg">
+                        <div class="staff-card-header">
+                            <img src="assets/img/staff/jack.jpg" alt="">
+                        </div>
+                        <div class="staff-card-body">
+                            <p class="staff-card-name text-center">Jack</p>
+                            <p class="staff-card-title text-center">Early Years Practitioner</p>
+                            <p class="staff-card-text">Jack is our newest member of the team joining us in May 2022 , having previously been with us on his student placements whilst at college. The children are thriving with his love of the outdoors- whatever the weather! Jack has completed his paediatric first aid and all relevant safeguarding qualifications. </p>
+                            <div class="text-center my-2"><button class="btn-primary staff-text">Read More</button></div>
+                        </div>
+                    </div>
+                    <div class="staff-card-lg">
+                        <div class="staff-card-header">
+                            <img src="assets/img/staff/chloe.jpg" alt="">
+                        </div>
+                        <div class="staff-card-body">
+                            <p class="staff-card-name text-center">Chloe</p>
+                            <p class="staff-card-title text-center">Early Years Practitioner Bank Staff </p>
+                            <p class="staff-card-text">Chloe joined us in January 2019 to complete her level 3 qualification in early years and now is with us as a qualified bank staff member. She also holds her level 3 qualification in teaching assisting and has experience in both schools and nurseries. She particularly enjoys creative and craft activities, and the children absolutely love creating their masterpieces with Chloe.  Chloe holds all relevant safeguarding training and is renewing her paediatric first aid in March 2023. </p>
+                            <div class="text-center my-2"><button class="btn-primary staff-text">Read More</button></div>
+                        </div>
+                    </div>
+                    <div class="staff-card-lg">
+                        <div class="staff-card-header">
+                            <img src="assets/img/staff/suzanne.jpg" alt="">
+                        </div>
+                        <div class="staff-card-body">
+                            <p class="staff-card-name text-center">Suzanne</p>
+                            <p class="staff-card-title text-center">Early Years Practitioner Bank Staff </p>
+                            <p class="staff-card-text">Suzanne joins us on a regular basis, which the children absolutely love. She has a passion for the outdoors- whatever the weather and is yet to spend a day inside since joining us over three years ago. She loves all kinds of physical and active play, and the children are fascinated with her imaginative and interactive games and story telling! She has full and relevant up to date first aid training and safeguarding training.</p>
+                            <div class="text-center my-2"><button class="btn-primary staff-text">Read More</button></div>
+                        </div>
+                    </div>
+                </div>
+                <p class="text-center my-2">All our practitioners have completed level 1 makaton training with intentions to continue this training onto level 2.</p>
+            </div>
+
+            </div>
+        </section>
+
+
+        <div class="staff-card-modal d-none" id="staff-card-modal">
+            <div class="staff-card-modal-body" id="staff-card-modal-body">
+
+                <div class="staff-card-body">
+                    <div class="staff-card-modal-close">
+                        <button class="btn-close"><i class="fa-solid fa-xmark"></i></button>
+                    </div>
+                    <p class="staff-card-name text-center" id="staff-card-modal-name"></p>
+                    <p class="staff-card-title text-center" id="staff-card-modal-title"></p>
+                    <p class="staff-card-text" id="staff-card-modal-text"></p>
+
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <?php include("inc/footer.inc.php"); ?>
+    <script>
+        $(".staff-text").on("click", function() {
+            var text = $(this).parents().siblings(".staff-card-text").html();
+            var staffname = $(this).parents().siblings(".staff-card-name").html();
+            var stafftitle = $(this).parents().siblings(".staff-card-title").html();
+            $("#staff-card-modal-text").html(text);
+            $("#staff-card-modal-name").html(staffname);
+            $("#staff-card-modal-title").html(stafftitle);
+            $("#staff-card-modal").fadeIn(400);
+        })
+        $(".btn-close").on("click", function() {
+            $('#staff-card-modal').fadeOut(400);
+        })
+    </script>
+</body>
