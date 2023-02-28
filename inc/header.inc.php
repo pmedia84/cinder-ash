fc5571b21935a095ce13ee62ba8cb8e09f44ea4f
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2023-02-28 09:37:06 +0000

    gtag

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -1,45 +1,48 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Favicon -->
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="manifest" href="/site.webmanifest">
-    <!-- Stylesheet -->
-    <link rel="stylesheet" href="assets/css/styles.css">
-    <!-- jQuery -->
-    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
-    <!-- Font Awesome -->
-    <script src="https://kit.fontawesome.com/3318fdaaaf.js" crossorigin="anonymous" defer></script>
-    <!-- Google Recaptcha -->
-    <script src="https://www.google.com/recaptcha/api.js?render=6LeRdqkkAAAAAHn11l-i3DDK9vgpi10iULGTpMHT" async="false" defer></script>
-    <!-- Google Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com" defer>
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin  defer>
-    <link href="https://fonts.googleapis.com/css2?family=Glory:wght@100;200;300;400;500;600;700;800&family=Jua&display=swap" rel="stylesheet"  defer>
-    <!-- Theme Color for safari and mobile browsers -->
-    <meta name="theme-color" content="#A67E6F" />
-    <!-- OG Meta Tags -->
-    <meta property="og:title" content="Cinder Ash">
-    <meta property="og:description" content="Nursery and Pre-School in Long Sutton">
-    <meta property="og:image" content="https://dev.parrotmedia.co.uk/assets/img/og-data/og-img.png">
-    <meta property="og:url" content="">
-    <meta property="og:type" content="Website">
-    <!-- /OG Meta Tags -->
-    <script type="application/ld+json">
-    {
-      "@context" : "https://schema.org",
-      "@type" : "WebSite",
-      "name" : "Cinder Ash Nursery and Pre-School",
-      "url" : "https://www.cinderashpreschool.co.uk/"
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Favicon -->
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+  <link rel="manifest" href="/site.webmanifest">
+  <!-- Stylesheet -->
+  <link rel="stylesheet" href="assets/css/styles.css">
+  <!-- jQuery -->
+  <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+  <!-- Font Awesome -->
+  <script src="https://kit.fontawesome.com/3318fdaaaf.js" crossorigin="anonymous" defer></script>
+  <!-- Google Recaptcha -->
+  <script src="https://www.google.com/recaptcha/api.js?render=6LeRdqkkAAAAAHn11l-i3DDK9vgpi10iULGTpMHT" async="false" defer></script>
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com" defer>
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin defer>
+  <link href="https://fonts.googleapis.com/css2?family=Glory:wght@100;200;300;400;500;600;700;800&family=Jua&display=swap" rel="stylesheet" defer>
+  <!-- Theme Color for safari and mobile browsers -->
+  <meta name="theme-color" content="#A67E6F" />
+  <!-- OG Meta Tags -->
+  <meta property="og:title" content="Cinder Ash">
+  <meta property="og:description" content="Nursery and Pre-School in Long Sutton">
+  <meta property="og:image" content="https://dev.parrotmedia.co.uk/assets/img/og-data/og-img.png">
+  <meta property="og:url" content="">
+  <meta property="og:type" content="Website">
+  <!-- /OG Meta Tags -->
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-XEFMTLT8NT"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
     }
+    gtag('js', new Date());
+
+    gtag('config', 'G-XEFMTLT8NT');
   </script>
 
 
-    <!-- Everything above this is for the head element. And is displayed on every web page -->
+  <!-- Everything above this is for the head element. And is displayed on every web page -->
