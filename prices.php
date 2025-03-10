d6d90e650cc9168debd3c65506d182d8cc22091c
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2025-03-10 19:23:01 +0000

    update 10-3-25

--- prices.php
+++ prices.php
@@ -20,22 +20,22 @@
         <section class="container my-4" id="section-one">
             <h2 class="section-title text-center">Our current fees</h2>
             <div class="std-card">
-            <table class="std-table">
+                <table class="std-table">
                     <tr class="table-head">
                         <th>Type</th>
                         <th>Amount</th>
-                    </tr>   
+                    </tr>
                     <tr>
                         <td>Under 2's</td>
-                        <td>&pound;5.10 Per hour</td>
+                        <td>&pound;5.35 Per hour</td>
                     </tr>
                     <tr>
                         <td>2 - 3 Year Old</td>
-                        <td>&pound;4.75 Per hour</td>
+                        <td>&pound;5.00 Per hour</td>
                     </tr>
                     <tr>
                         <td>3 Years and above</td>
-                        <td>&pound;4.50 Per hour</td>
+                        <td>&pound;4.75 Per hour</td>
                     </tr>
                     <tr>
                         <td>EYE Funding</td>
@@ -44,7 +44,15 @@
 
                 </table>
                 <p class="my-2">Invoices are sent out at the beginning of each month for payment within 14 days.</p>
-                <p>Children accessing all year round fee paying contracts can request 4 weeks of holiday leave, only charged at 50% (this does not apply to term time only children).</p>
+                <p>Whilst your children are here with us at Cinder Ash it is our duty to ensure we are upholding high standards of care and learning. In order for the children to have a good amount of energy we provide a wonderful filling and balance snack twice a day, one in the morning and one in the afternoon.</p>
+                <p>The children are always provided with a carbohydrate food that is long lasting with them being very active in the setting such as crumpet, toast, crackers, pitta bread or breadsticks with a side of fruit. </p>
+                <p>We kindly ask parents to support us in ensuring their children access a range of food and take part in our snack times where not only do they get to learn how to self serve, use it as a social event building on communication but also trying new foods alongside their friends and with adult modelling. 
+                With this we kindly ask parents depending on how many sessions a week their child does to contribute voluntarily to the snack costs. </p>
+                <p>Contributions are to be done termly and will be invoiced. (September-December. January-March. April- July). 
+                If your child does less that 2.5 sessions a week the cost for a full term is £5.00. (Maximum amount £15.00 for the full academic year)</p>
+                <p>If your child does over 2.5 sessions a week the cost for a full term is &pound;10.00 (Maximum &pound;30.00 for the full academic year)
+                We kindly ask parents if they wish to opt out then a piece of fruit for snacks must be provided. </p>
+
             </div>
 
         </section>
