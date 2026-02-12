38c78fee54d9a14425d0e62778008b8a6a67f3d6
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2026-02-12 10:52:28 +0000

    Refactor code structure for improved readability and maintainability

--- policies.php
+++ policies.php
@@ -17,18 +17,26 @@
             </div>
         </div>
 
-        <section class="container my-4" id="section-one">
-            <h2 class="section-title text-center">Documents</h2>
-            <p class="text-center section-subtitle mb-3">Download our policy documents</p>
+        <section class="container my-4">
             <div class="std-card">
-                <p>Click on the links below to download the documents</p>
-                <ul class="files">
-                    <li><a href="assets/docs/child-protection-procedure.docx" download="child-protection.docx"><i class="fa-regular fa-file-word"></i> Child Protection</a></li>
-                    <li><a href="assets/docs/complaints-procedure.docx" download="complaints-procedure.docx"><i class="fa-regular fa-file-word"></i> Complaints Procedure</a></li>
-                    <li><a href="assets/docs/safeguarding-children.docx" download="safeguarding-children.docx"><i class="fa-regular fa-file-word"></i> Safeguarding Children</a></li>
-                </ul>
+                <div class="row">
+                    <div class="col-50">
+                        <h2 class="section-title text-left">Documents</h2>
+                        <p class="text-left section-subtitle mb-3">Download our policy documents</p>
+                        <p>Click on the links below to download the documents</p>
+                        <ul class="files">
+                            <li><a href="assets/docs/child-protection-procedure.docx" download="child-protection.docx"><i class="fa-regular fa-file-word"></i> Child Protection</a></li>
+                            <li><a href="assets/docs/complaints-procedure.docx" download="complaints-procedure.docx"><i class="fa-regular fa-file-word"></i> Complaints Procedure</a></li>
+                            <li><a href="assets/docs/safeguarding-children.docx" download="safeguarding-children.docx"><i class="fa-regular fa-file-word"></i> Safeguarding Children</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-50">
+                        <img class="border-std" src="/assets/img/encompass-graphic.webp" alt="Encompass Graphic">
+                        <p>if you wish to contact Lincolnshire children's safeguarding partnership to raise concerns, contact details are as follows:</p>
+                        <p><a href="https://www.lincolnshirescp.org.uk/" target="_blank">Lincolnshire (LSCP)</a> <a href="tel:01522782111">01522 782111</a> (Mon-Fri 8am-6pm) or <a href="tel:01522 782333">01522 782333</a> (out of hours).</p>
+                    </div>
+                </div>
             </div>
-
         </section>
     </main>
 
